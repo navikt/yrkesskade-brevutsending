b92bd8eeb914a6d495a709018b3531f9ccdc3573
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-06-20 13:01:22 +0200

    YSMOD-420 Initial commit

---
+++ src/main/kotlin/no/nav/yrkesskade/brevutsending/YrkesskadeBrevutsendingApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.yrkesskade.brevutsending
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YrkesskadeBrevutsendingApplication
+
+fun main(args: Array<String>) {
+	runApplication<YrkesskadeBrevutsendingApplication>(*args)
+}

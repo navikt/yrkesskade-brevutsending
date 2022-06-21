a129d393f340300139cbe8f9285077675b3d66fa
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-06-21 10:59:57 +0200

    YSMOD-420 Domeneklasser for dokdist

---
+++ src/main/kotlin/no/nav/yrkesskade/brevutsending/domene/Dokdist.kt
@@ -0,0 +1,20 @@
+package no.nav.yrkesskade.brevutsending.domene
+
+data class DistribuerJournalpostRequest(
+    val journalpostId: String,
+    val bestillendeFagsystem: String = "yrkesskade-saksbehandling",//Fagsystemet som bestiller distribusjon. Felles kode for fagsystemet. Konsumenter bestemmer selv verdi, men må være lik for alle forsendelser fra fagsystemet.
+    val dokumentProdApp: String = "yrkesskade-brevutsending", //Applikasjon som har produsert hoveddokumentet (for sporing og feilsøking)
+    val distribusjonstype: String = Distribusjonstype.ANNET.name,
+    val distribusjonstidspunkt: String = Distribusjonstidspunkt.KJERNETID.name
+)
+
+enum class Distribusjonstype {
+    VEDTAK,
+    VIKTIG,
+    ANNET
+}
+
+enum class Distribusjonstidspunkt {
+    UMIDDELBART, // hele døgnet
+    KJERNETID // 7-23
+}
